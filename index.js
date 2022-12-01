31ace46ef7f99e599caa4dec0a689a04408e11bc
Dustin Lammiman <dustin@nosecreekweb.ca>
2022-12-01 10:57:12 -0700

    Allows running specific day using arguments

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 day1a = require('./calendar/day1a')
 day1b = require('./calendar/day1b')
 
-// process.argv[2]
-console.log(day1b())
+const days = { day1a: day1a(), day1b: day1b() }
+const dayToRun = days?.[process.argv[2]] || days[Object.keys(days).pop()]
+console.log(dayToRun)
