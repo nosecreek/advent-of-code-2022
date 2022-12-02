cf3273a101409915998ec97faf28e2570e0f978d
Dustin Lammiman <dustin@nosecreekweb.ca>
2022-12-02 10:47:35 -0700

    Day 2 Part 1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 day1a = require('./calendar/day1a')
 day1b = require('./calendar/day1b')
+day2a = require('./calendar/day2a')
 
-const days = { day1a: day1a(), day1b: day1b() }
+const days = { day1a: day1a(), day1b: day1b(), day2a: day2a() }
 const dayToRun = days?.[process.argv[2]] || days[Object.keys(days).pop()]
 console.log(dayToRun)
