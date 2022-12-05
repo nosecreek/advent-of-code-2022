d42363c49a24e1790147776be98cf0ad9a959a8d
Dustin Lammiman <dustin@nosecreekweb.ca>
2022-12-05 12:37:08 -0700

    Day 5 Part 2

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const day3b = require('./calendar/day3b')
 const day4a = require('./calendar/day4a')
 const day4b = require('./calendar/day4b')
 const day5a = require('./calendar/day5a')
+const day5b = require('./calendar/day5b')
 
 const days = {
   day1a: day1a(),
@@ -17,7 +18,8 @@ const days = {
   day3b: day3b(),
   day4a: day4a(),
   day4b: day4b(),
-  day5a: day5a()
+  day5a: day5a(),
+  day5b: day5b()
 }
 const dayToRun = days?.[process.argv[2]] || days[Object.keys(days).pop()]
 console.log(dayToRun)
